3b66a3047100a288115f60a1721bc9cc8143e0ac
trzeciak20p <91263438+trzeciak20p@users.noreply.github.com>
2022-01-24 11:09:25 +0100

    prevent default strzałek

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ const main = document.getElementById("main")
 const score_counter = document.getElementById("score_counter")
 const best_score = document.getElementById("score_best")
 
-
-
+//poruszanie
+document.addEventListener("keydown", function (event) {         
+    if (event.keyCode >= 37 && event.keyCode <=40) {          //blokowanie jeżdżenia po stronie strzałkami
+        event.preventDefault()
+    }
+}
 
 function start_game(){
 
