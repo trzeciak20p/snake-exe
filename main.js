82e25c91d42f27073921b940847e0d5c3a8b2b45
trzeciak20p <m.trzeciak.p20@gmail.com>
2022-02-12 20:32:04 +0100

    board is here

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const main = document.getElementById("main")
 const score_counter = document.getElementById("score_counter")
 const best_score = document.getElementById("score_best")
 
+const cols = 20
+const rows = 20
 
 
 
@@ -10,14 +12,35 @@ function start_game(){
     
 }
 
-function main(){
+function main_start(){
     
-    best_score.innerText = 
+    //best_score.innerText = 
 
+    //tiles creating
+    for(i = 0; i < rows; i++){
+        
+        for(j = 0; j < cols;j++){
+            tile = document.createElement("DIV")
+            if(i%2){
+                if(j%2){
+                    tile.setAttribute("class", "tile tile1")
+                }else{
+                    tile.setAttribute("class", "tile tile2")
+                }
+            }else{
+                if(j%2){
+                    tile.setAttribute("class", "tile tile2")
+                }else{
+                    tile.setAttribute("class", "tile tile1")
+                }
+            }
+            main.append(tile)
+        }
+    }
 
     start_game()
  
     return;
 }
 
-main()
+main_start()
